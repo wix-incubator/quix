63fe1251fff0d17fdd631ada68e4249b5b59d60a
Valery Frolov <valeryf@wix.com>
2019-04-14 21:21:43 +0300

    quix-backend : allow cors on db controller

--- quix-backend/quix-webapps/quix-web-spring/src/main/scala/quix/web/controllers/DbController.scala
+++ quix-backend/quix-webapps/quix-web-spring/src/main/scala/quix/web/controllers/DbController.scala
@@ -1,19 +1,21 @@
 package quix.web.controllers
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{PathVariable, RequestMapping, RequestMethod, ResponseBody}
+import org.springframework.web.bind.annotation._
 import quix.api.db.Db
 
 @Controller
 @RequestMapping(Array("/api"))
 class DbController(db: Db) {
 
+  @CrossOrigin(origins = Array("*"), allowedHeaders = Array("*"))
   @RequestMapping(value = Array("/db/explore"), method = Array(RequestMethod.GET))
   @ResponseBody
   def getCatalogsNoColumns = {
     db.catalogs
   }
 
+  @CrossOrigin(origins = Array("*"), allowedHeaders = Array("*"))
   @RequestMapping(value = Array("/db/explore/{catalog}/{schema}/{table}"), method = Array(RequestMethod.GET))
   @ResponseBody
   def getTable(@PathVariable catalog: String, @PathVariable schema: String, @PathVariable table: String) = {
