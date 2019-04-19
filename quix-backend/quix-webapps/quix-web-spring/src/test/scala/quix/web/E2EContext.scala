f3c2445a76982614e1d957fbaf6380c7a5a84257
Valery Frolov <valeryf@wix.com>
2019-04-19 19:36:19 +0300

    backend : minor

--- quix-backend/quix-webapps/quix-web-spring/src/test/scala/quix/web/E2EContext.scala
+++ quix-backend/quix-webapps/quix-web-spring/src/test/scala/quix/web/E2EContext.scala
@@ -1,12 +1,16 @@
 package quix.web
 
-import quix.core.utils.StringJsonHelpersSupport
 import quix.core.utils.JsonOps.Implicits.global
+import quix.core.utils.StringJsonHelpersSupport
 import scalaj.http.Http
 
 trait E2EContext extends StringJsonHelpersSupport {
 
   def get[T: Manifest](url: String): T = {
     Http("http://localhost:8888/" + url).asString.body.as[T]
+  }
+
+  def getResponse(url: String) = {
+    Http("http://localhost:8888/" + url).asString
   }
 }
