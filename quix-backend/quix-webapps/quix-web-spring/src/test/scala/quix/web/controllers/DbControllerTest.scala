a75a2468f6a3b16dfcae45973a2812942cb76923
Valery Frolov <valeryf@wix.com>
2019-04-19 19:37:25 +0300

    backend : add tests for DbController.table endpoint

--- quix-backend/quix-webapps/quix-web-spring/src/test/scala/quix/web/controllers/DbControllerTest.scala
+++ quix-backend/quix-webapps/quix-web-spring/src/test/scala/quix/web/controllers/DbControllerTest.scala
@@ -8,12 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.{TestContextManager, TestPropertySource}
-import quix.api.db.{Catalog, Schema, Table}
+import quix.api.db.{Catalog, Kolumn, Schema, Table}
 import quix.web.{E2EContext, SpringConfigWithTestExecutor, TestQueryExecutorInstance}
 
 @RunWith(classOf[SpringRunner])
 @SpringBootTest(webEnvironment = DEFINED_PORT, classes = Array(classOf[SpringConfigWithTestExecutor]))
-@TestPropertySource(locations=Array("classpath:test.properties"))
+@TestPropertySource(locations = Array("classpath:test.properties"))
 class DbControllerTest extends E2EContext {
 
   new TestContextManager(this.getClass).prepareTestInstance(this)
@@ -37,6 +37,28 @@ class DbControllerTest extends E2EContext {
     val catalog = Catalog("catalog", children = List(Schema("schema", children = List(Table("table", children = Nil)))))
 
     assertThat(catalogs, Matchers.is(List(catalog)))
+  }
+
+  @Test
+  def handleGetTableRequestWhenPrestoIsOnline(): Unit = {
+    executor.clear.withResults(List(List("column1", "varchar"), List("column2", "int")))
+
+    val actual = get[Table]("api/db/explore/catalog/schema/table")
+
+    val expected = Table("table", List(Kolumn("column1", "varchar"), Kolumn("column2", "int")))
+
+    assertThat(actual, Matchers.is(expected))
+  }
+
+  @Test
+  def handleGetTableRequestWhenPrestoIsDown(): Unit = {
+    executor.clear.withException(new Exception("presto is down!"))
+
+    val actual = get[Table]("api/db/explore/catalog/schema/table")
+
+    val expected = Table("table", children = Nil)
+
+    assertThat(actual, Matchers.is(expected))
   }
 }
 
