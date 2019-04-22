dab1d722a04d326d64ca950495e86d6529561895
Valery Frolov <valeryf@wix.com>
2019-04-22 22:02:28 +0300

    backend : replace hardcoded value for flaky tests with busy wait for first message

--- quix-backend/quix-webapps/quix-web-spring/src/test/scala/quix/web/E2EContext.scala
+++ quix-backend/quix-webapps/quix-web-spring/src/test/scala/quix/web/E2EContext.scala
@@ -31,7 +31,9 @@ trait E2EContext extends StringJsonHelpersSupport {
     val handler = new WebSocketUpgradeHandler.Builder().addWebSocketListener(listener).build()
 
     c.prepareGet("ws://localhost:8888/api/v1/execute/sql").execute(handler)
-    Thread.sleep(1000)
+
+    while(listener.messages.isEmpty)
+      Thread.sleep(10)
 
     if (awaitFinish) {
       while (!listener.closed)
