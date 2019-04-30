d5bb51982e491187610409d62c63e16bd6e0f829
Anton Podolsky <antonp@wix.com>
2019-04-30 10:40:25 +0300

    frontend: assign ids to db tree items

--- quix-frontend/client/src/services/db.ts
+++ quix-frontend/client/src/services/db.ts
@@ -1,15 +1,18 @@
 import {IFile} from '../../../shared';
+import {utils} from '../lib/core';
 
 export const convert = (nodes: any[], path = [], res = []) => {
   nodes.forEach(node => {
-    if (!node.children) {
-      res.push({id: node.name, name: node.name, path, type: 'column', nodeType: node.dataType});
-    } else if (!node.children.length && node.type === 'table') {
-      res.push({id: node.name, name: node.name, path, type: 'folder', nodeType: node.type, lazy: true});
-    } else {
-      res.push({id: node.name, name: node.name, path, type: 'folder', nodeType: node.type});
+    const id = utils.uuid();
 
-      convert(node.children, [...path, {id: node.name, name: node.name}], res);
+    if (!node.children) {
+      res.push({id, name: node.name, path, type: 'column', nodeType: node.dataType});
+    } else if (!node.children.length && node.type === 'table') {
+      res.push({id, name: node.name, path, type: 'folder', nodeType: node.type, lazy: true});
+    } else {
+      res.push({id, name: node.name, path, type: 'folder', nodeType: node.type});
+
+      convert(node.children, [...path, {id, name: node.name}], res);
     }
   });
 
