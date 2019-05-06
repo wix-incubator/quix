a550aa273fef86d05f040a6c4d197c96ea97c8bc
Yaara Cohen <yaarac@wix.com>
2019-05-06 11:49:24 +0300

    Echarts - Theme + zoom (#73)
    * echarts dataZoom and padding
    
    * themes

--- quix-frontend/client/src/lib/viz/directives/chart/chart-renderer.ts
+++ quix-frontend/client/src/lib/viz/directives/chart/chart-renderer.ts
@@ -1,11 +1,15 @@
 import {IMeta, IData, IFilterData} from '../../services/chart/chart-conf';
 import {isDimension, isDate} from '../../services/chart/chart-utils';
 import * as echarts from 'echarts/lib/echarts';
-import 'echarts/lib/chart/line'
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/title'
-import 'echarts/lib/component/legend'
+import 'echarts/lib/chart/line';
+import 'echarts/lib/chart/bar';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/legend';
+import 'echarts/lib/component/dataZoom';
+import {default as essos} from '../theme';
+
+echarts.registerTheme('essos', essos);
 
 function getChartType(filter: IFilterData, meta: IMeta) {
   if (isDimension(filter.x, meta)) {
@@ -46,8 +50,7 @@ export class ChartRenderer {
 
     const maxInterval = getMaxInterval(xAxisType, data);
     data.map(series => (series as any).type = chartType);
-
-    this.chart = echarts.init(this.container.get(0));
+    this.chart = echarts.init(this.container.get(0), 'essos');
     this.chart.clear();
     this.chart.setOption({
       tooltip: {
@@ -58,17 +61,33 @@ export class ChartRenderer {
         data: data.map((series: any) => series.name),
         bottom: true
       },
+      grid: {
+        top: 10,
+        right: 10
+      },
+      dataZoom: [
+        {
+          show: xAxisType !== 'category',
+          realtime: true,
+          start: 20,
+          end: 85
+        },
+        {
+          type: 'inside',
+          realtime: true,
+          start: 65,
+          end: 85
+        }
+      ],
       xAxis: {
         type: xAxisType,
         maxInterval,
-        name: filter.x,
         nameLocation: 'center',
         nameTextStyle: {
           lineHeight: 40
         }
       },
       yAxis: {
-        name: filter.y.length === 1 ? filter.y[0] : null,
         nameLocation: 'center',
         nameTextStyle: {
           lineHeight: 40
