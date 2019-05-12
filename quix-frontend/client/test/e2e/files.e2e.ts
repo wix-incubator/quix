b7fc3587e4feae119ed4165ed38eee17171ce152
Anton Podolsky <antonp@wix.com>
2019-05-12 20:11:25 +0300

    frontend: refactor tests

--- quix-frontend/client/test/e2e/files.e2e.ts
+++ quix-frontend/client/test/e2e/files.e2e.ts
@@ -6,6 +6,43 @@ import {FilesTestkit} from '../../src/state-components/files/files-testkit';
 describe('Files ::', () => {
   let driver: Driver, testkit: FilesTestkit;
 
+  const gotoErrorFiles = async () => {
+    await driver.mock.http('/api/files', [404, {message: 'Failed to fetch files'}]);
+    await driver.goto(`/files/`);
+  }
+
+  const gotoErrorFolder = async () => {
+    await driver.mock.http('/api/files/:id', [404, {message: 'Folder not found'}]);
+    await driver.goto(`/files/1`);
+  }
+
+  const gotoEditableRootFolder = async (files = [createMockFolder(), createMockFile()]) => {
+    const mock = createMockFiles(files);
+
+    await driver.mock.http('/api/files', mock);
+    await driver.goto('/files/');
+
+    return mock;
+  }
+
+  const gotoEditableFolder = async (files = [createMockFolder(), createMockFile()]) => {
+    const mock = createMockFolderPayload(files);
+
+    await driver.mock.http('/api/files/:id', mock);
+    await driver.goto('/files/1');
+
+    return mock;
+  }
+
+  const gotoReadonlyFolder = async (files = [createMockFolder(), createMockFile()]) => {
+    const mock = createMockFolderPayload(files, {owner: 'readonly@quix.com'});
+
+    await driver.mock.http('/api/files/:id', mock);
+    await driver.goto('/files/1');
+
+    return mock;
+  }
+
   beforeEach(async () => {
     driver = new Driver();
     await driver.init();
@@ -14,22 +51,19 @@ describe('Files ::', () => {
   });
 
   it('should display error state when failed to fetch files', async () => {
-    await driver.mock.http('/api/files', [404, {message: 'Failed to fetch files'}]);
-    await driver.goto(`/files/`);
+    await gotoErrorFiles();
 
     expect(await testkit.hasErrorState()).to.be.true;
   });
 
   it('should display error state when folder is not found', async () => {
-    await driver.mock.http('/api/files/:id', [404, {message: 'Folder not found'}]);
-    await driver.goto(`/files/1`);
+    await gotoErrorFolder();
 
     expect(await testkit.hasErrorState()).to.be.true;
   });
 
   it('should display empty state in root folder when user does not have folders/notebooks', async () => {
-    await driver.mock.http('/api/files', createMockFiles());
-    await driver.goto('/files/');
+    await gotoEditableRootFolder([]);
 
     const breadcrumbsTestkit = await testkit.getBreadcrumbsTestkit();
 
@@ -38,8 +72,7 @@ describe('Files ::', () => {
   });
 
   it('should display empty state when folder does not have folders/notebooks', async () => {
-    await driver.mock.http('/api/files/:id', createMockFolderPayload());
-    await driver.goto('/files/1');
+    await gotoEditableFolder([]);
 
     const breadcrumbsTestkit = await testkit.getBreadcrumbsTestkit();
 
@@ -48,77 +81,68 @@ describe('Files ::', () => {
   });
 
   it('should display content in root folder when user has folders/notebooks', async () => {
-    await driver.mock.http('/api/files', createMockFiles([createMockFile()]));
-    await driver.goto('/files/');
+    await gotoEditableRootFolder();
 
     expect(await testkit.hasContent()).to.be.true;
   });
 
   it('should display content when folder has folders/notebooks', async () => {
-    await driver.mock.http('/api/files/:id', createMockFolderPayload([createMockFile()]));
-    await driver.goto('/files/1');
+    await gotoEditableFolder();
 
     expect(await testkit.hasContent()).to.be.true;
   });
 
   it('should navigate to a notebook in the root folder', async () => {
-    const files = createMockFiles([createMockFile()]);
+    const files = await gotoEditableRootFolder();
 
-    await driver.mock.http('/api/files', files);
-    await driver.goto('/files/');
-    await testkit.clickFile(1);
+    await testkit.clickFile(2);
 
-    expect(await driver.url.matches(`/notebook/${files[1].id}`)).to.be.true;
+    expect(await driver.url.matches(`/notebook/${files[2].id}`)).to.be.true;
   });
 
   it('should navigate to a notebook in a folder', async () => {
-    const folder = createMockFolderPayload([createMockFile()]);
+    const folder = await gotoEditableFolder();
 
-    await driver.mock.http('/api/files/:id', folder);
-    await driver.goto('/files/1');
-    await testkit.clickFile(1);
+    await testkit.clickFile(2);
 
-    expect(await driver.url.matches(`/notebook/${folder.files[0].id}`)).to.be.true;
+    expect(await driver.url.matches(`/notebook/${folder.files[1].id}`)).to.be.true;
   });
 
   it('should navigate to a folder in the root folder', async () => {
-    const files = createMockFiles([createMockFolder()]);
+    const files = await gotoEditableRootFolder();
 
-    await driver.mock.http('/api/files', files);
-    await driver.goto('/files/');
     await testkit.clickFile(1);
 
     expect(await driver.url.matches(`/files/${files[1].id}`)).to.be.true;
   });
 
   it('should navigate to a folder in a folder', async () => {
-    const folder = createMockFolderPayload([createMockFolder()]);
+    const folder = await gotoEditableFolder();
 
-    await driver.mock.http('/api/files/:id', folder);
-    await driver.goto('/files/1');
     await testkit.clickFile(1);
 
     expect(await driver.url.matches(`/files/${folder.files[0].id}`)).to.be.true;
   });
 
   it('should create a folder in a root folder', async () => {
-    await driver.mock.http('/api/files', createMockFiles());
-    await driver.goto('/files/');
+    await gotoEditableRootFolder([]);
+
     await testkit.clickAddFolder();
 
     expect(await testkit.numOfFiles()).to.equal(1);
   });
 
   it('should create a folder in a folder', async () => {
-    await driver.mock.http('/api/files/:id', createMockFolderPayload());
-    await driver.goto('/files/1');
+    await gotoEditableFolder([]);
+
     await testkit.clickAddFolder();
 
     expect(await testkit.numOfFiles()).to.equal(1);
   });
 
   it.skip('should create a notebook in a root folder and navigate to it', async () => {
-    await driver.goto('/files/');
+    await gotoEditableRootFolder([]);
+
     await testkit.clickAddNotebook();
 
     const breadcrumbsTestkit = await testkit.getBreadcrumbsTestkit();
@@ -128,7 +152,8 @@ describe('Files ::', () => {
   });
 
   it.skip('should create a notebook in a folder and navigate to it', async () => {
-    await driver.goto('/files/1');
+    await gotoEditableFolder([]);
+
     await testkit.clickAddNotebook();
 
     const breadcrumbsTestkit = await testkit.getBreadcrumbsTestkit();
@@ -138,7 +163,8 @@ describe('Files ::', () => {
   });
 
   it('should navigate to the root after navigating to a folder and clicking the root folder', async () => {
-    await driver.goto('/files/');
+    await gotoEditableRootFolder();
+
     await testkit.clickFile(1);
 
     expect(await driver.url.matches('/files/:id')).to.be.true;
@@ -152,10 +178,7 @@ describe('Files ::', () => {
   describe('Permissions ::', () => {
     describe('Name ::', () => {
       it('should allow to edit name if user is owner', async () => {
-        const folder = createMockFolderPayload([]);
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoEditableFolder();
 
         const breadcrumbsTestkit = await testkit.getBreadcrumbsTestkit();
 
@@ -163,10 +186,7 @@ describe('Files ::', () => {
       });
 
       it('should not allow to edit name if user is not owner', async () => {
-        const folder = createMockFolderPayload([], {owner: 'readonly@quix.com'});
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoReadonlyFolder();
 
         const breadcrumbsTestkit = await testkit.getBreadcrumbsTestkit();
 
@@ -176,20 +196,14 @@ describe('Files ::', () => {
 
     describe('Add folder | Add notebook ::', () => {
       it('should enable "Add folder" and "Add notebook" buttons if user is owner', async () => {
-        const folder = createMockFolderPayload([]);
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoEditableFolder();
 
         expect(await testkit.isAddFolderEnabled()).to.be.true;
         expect(await testkit.isAddNotebookEnabled()).to.be.true;
       });
 
       it('should disable "Add folder" and "Add notebook" buttons if user is not owner', async () => {
-        const folder = createMockFolderPayload([], {owner: 'readonly@quix.com'});
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoReadonlyFolder();
 
         expect(await testkit.isAddFolderEnabled()).to.be.false;
         expect(await testkit.isAddNotebookEnabled()).to.be.false;
@@ -198,10 +212,7 @@ describe('Files ::', () => {
 
     describe('Delete ::', () => {
       it('should enable the delete action if user is owner', async () => {
-        const folder = createMockFolderPayload([]);
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoEditableFolder();
 
         const actionsTestkit = await testkit.getActionsTestkit();
 
@@ -209,10 +220,7 @@ describe('Files ::', () => {
       });
 
       it('should disable the delete action if user is not owner', async () => {
-        const folder = createMockFolderPayload([], {owner: 'readonly@quix.com'});
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoReadonlyFolder();
 
         const actionsTestkit = await testkit.getActionsTestkit();
 
@@ -222,19 +230,13 @@ describe('Files ::', () => {
 
     describe('Bulk select ::', () => {
       it('should enable bulk selection if user is owner', async () => {
-        const folder = createMockFolderPayload([createMockFile()]);
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+        await gotoEditableFolder();
 
         expect(await testkit.isBulkSelectEnabled()).to.be.true;
       });
 
-      it('should disable bulk selection if user is owner', async () => {
-        const folder = createMockFolderPayload([createMockFile()], {owner: 'readonly@quix.com'});
-
-        await driver.mock.http(`/api/files/:id`, folder);
-        await driver.goto('/files/1');
+      it('should disable bulk selection if user is not owner', async () => {
+        await gotoReadonlyFolder();
 
         expect(await testkit.isBulkSelectEnabled()).to.be.false;
       });
