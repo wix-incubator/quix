4a97219bc6a704868c4dfc9e06bb289f11c29a9e
Anton Podolsky <antonp@wix.com>
2019-05-12 20:33:55 +0300

    frontend: add case-insensitive match highlighting

--- quix-frontend/client/src/services/search.ts
+++ quix-frontend/client/src/services/search.ts
@@ -1,8 +1,10 @@
 import {findIndex} from 'lodash';
 
-export const extractTextAroundMatch = (text: string, match: string, numOfWrappingLines = 1) => {
+export const extractTextAroundMatch = (text: string, match: string, numOfWrappingLines = 2) => {
+  match = match.toLowerCase();
+
   const lines = text.split('\n');
-  const index = findIndex(lines, line => line.indexOf(match) !== -1);
+  const index = findIndex(lines, line => line.toLowerCase().indexOf(match) !== -1);
 
   return lines
     .slice(Math.max(0, index - numOfWrappingLines), Math.min(lines.length, index + 1 + numOfWrappingLines))
