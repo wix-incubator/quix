ca078a4fb919793d1544fdd2d303e99d0554ae49
Valery Frolov <valeryf@wix.com>
2019-06-15 20:20:12 +0300

    backend : minor

--- quix-backend/quix-webapps/quix-web-spring/src/main/scala/quix/web/controllers/DbController.scala
+++ quix-backend/quix-webapps/quix-web-spring/src/main/scala/quix/web/controllers/DbController.scala
@@ -1,11 +1,10 @@
 package quix.web.controllers
 
-import monix.execution.Scheduler
+import monix.execution.Scheduler.Implicits.global
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation._
 import quix.api.db.Db
 
-import scala.concurrent.Await
 import scala.concurrent.duration.FiniteDuration
 
 @Controller
@@ -16,20 +15,20 @@ class DbController(db: Db, requestTimeout: FiniteDuration) {
   @RequestMapping(value = Array("/db/explore"), method = Array(RequestMethod.GET))
   @ResponseBody
   def getCatalogsNoColumns = {
-    Await.result(db.catalogs.runToFuture(Scheduler.global), requestTimeout)
+    db.catalogs.runSyncUnsafe(requestTimeout)
   }
 
   @CrossOrigin(origins = Array("*"), allowedHeaders = Array("*"))
   @RequestMapping(value = Array("/db/explore/{catalog}/{schema}/{table}"), method = Array(RequestMethod.GET))
   @ResponseBody
   def getTable(@PathVariable catalog: String, @PathVariable schema: String, @PathVariable table: String) = {
-    Await.result(db.table(catalog, schema, table).runToFuture(Scheduler.global), requestTimeout)
+    db.table(catalog, schema, table).runSyncUnsafe(requestTimeout)
   }
 
   @CrossOrigin(origins = Array("*"), allowedHeaders = Array("*"))
   @RequestMapping(value = Array("/db/autocomplete"), method = Array(RequestMethod.GET))
   @ResponseBody
   def autocomplete() = {
-    Await.result(db.autocomplete.runToFuture(Scheduler.global), requestTimeout)
+    db.autocomplete.runSyncUnsafe(requestTimeout)
   }
 }
