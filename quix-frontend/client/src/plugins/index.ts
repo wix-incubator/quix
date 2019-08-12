873d11b8c25efb0ad5418ae451786bbb6b0c49ce
Anton Podolsky <antonp@wix.com>
2019-08-12 20:52:14 +0300

    minor

--- quix-frontend/client/src/plugins/index.ts
+++ quix-frontend/client/src/plugins/index.ts
@@ -3,5 +3,5 @@ import {pluginFactory} from './plugin-factory';
 import {PluginManager} from '../services/plugins';
 
 export const pluginManager = new PluginManager(pluginFactory, {
-  import: new AsyncSeriesHook(['store', 'note', 'questionId']),
+  import: new AsyncSeriesHook(['store', 'note', 'value']),
 });
