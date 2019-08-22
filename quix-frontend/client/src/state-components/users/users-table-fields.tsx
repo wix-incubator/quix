cf0280d973ae11e8b57b451e4ce1c81cc97601f1
Shahar Levi <shaharl@wix.com>
2019-08-22 13:18:42 +0300

    create a generic table component and use it in users component (#150)

---
+++ quix-frontend/client/src/state-components/users/users-table-fields.tsx
@@ -0,0 +1,44 @@
+import {IUser} from '../../../../shared';
+import biRelativeDate from '../../../src/lib/ui/filters/relative-date';
+import * as React from 'react';
+import {RowConfig} from '../../lib/ui/components/Table';
+
+export const usersTableFields: RowConfig<IUser>[] = [
+  {
+    name: 'name',
+    title: 'user',
+    filter(_, user: IUser, index) {
+      return (
+        <div className="bi-align bi-s-h">
+          <img className="quix-user-avatar" src={user.avatar} />
+          <span>{user.name}</span>
+        </div>
+      );
+    }
+  },
+  {
+    name: 'email'
+  },
+  {
+    name: 'dateCreated',
+    title: 'Join Date',
+    filter(_, user: IUser, index) {
+      return (
+        <span className="bi-text--sm bi-muted">
+          {biRelativeDate()(user.dateCreated as any)}
+        </span>
+      );
+    }
+  },
+  {
+    name: 'dateUpdated',
+    title: 'Last Login',
+    filter(_, user: IUser, index) {
+      return (
+        <span className="bi-text--sm bi-muted">
+          {biRelativeDate()(user.dateUpdated as any)}
+        </span>
+      );
+    }
+  }
+];
