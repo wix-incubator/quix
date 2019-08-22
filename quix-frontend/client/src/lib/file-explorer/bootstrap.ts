a0ac313e70121032d131e92cfab6de5ac1c7566d
Anton Podolsky <antonp@wix.com>
2019-08-22 17:48:22 +0300

    revert last commit

--- quix-frontend/client/src/lib/file-explorer/bootstrap.ts
+++ quix-frontend/client/src/lib/file-explorer/bootstrap.ts
@@ -6,8 +6,4 @@ import 'angular-dragdrop';
 import '../core';
 import '../ui';
 
-import scrollMonitor from 'scrollmonitor';
-(window as any).scrollMonitor = scrollMonitor;
-import 'ap-viewport-watch';
-
-export default angular.module('bi.fileExplorer', ['bi.core', 'bi.ui', 'ngDragDrop', 'angularViewportWatch']);
+export default angular.module('bi.fileExplorer', ['bi.core', 'bi.ui', 'ngDragDrop']);
