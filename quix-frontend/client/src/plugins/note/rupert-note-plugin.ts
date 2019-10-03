26b852599b422ae5c8a4c7fc7adde7d5e31b8a1f
Anton Podolsky <antonp@wix.com>
2019-10-03 12:50:32 +0300

    add runFinish hook

--- quix-frontend/client/src/plugins/note/rupert-note-plugin.ts
+++ quix-frontend/client/src/plugins/note/rupert-note-plugin.ts
@@ -5,14 +5,16 @@ import { Store } from '../../lib/store';
 import { NotePlugin} from '../../services/plugins';
 import { ParamParser } from '../../lib/code-editor/services/param-parser';
 import { DefaultParamSerializer } from '../../lib/code-editor/services/param-parser/param-serializers/default-param-serializer';
+import { Runner } from '../../lib/runner';
 
-const generateNoteContent = ({requestor, question, queries}) => {
+const generateNoteContent = (questionId, {requestor, question, queries}) => {
   const parser = new ParamParser(new DefaultParamSerializer());
   const params = flatten(queries.map(query => query.params))
     .map(({name, type, value}) => parser.createParam(name, type, value));
 
   const header = `
 /**************************************
+Question ID: ${questionId}
 Submitted by: ${requestor}
 Question: ${question}
 **************************************/`;
@@ -30,15 +32,46 @@ export class RupertNotePlugin extends NotePlugin {
     });
 
     this.type = 'presto';
+
+    const baseRupertApiUrl = `${app.getConfig().getClientTopology().apiBasePath}/api/module/rupert`;
     
     hooks.import.tapPromise('RupertNotePlugin', (store: Store, note: INote, questionId: string) => {
-      if (note.type === ModuleEngineType.Rupert) {
-        return fetch(`${app.getConfig().getClientTopology().apiBasePath}/api/module/rupert/question/${encodeURIComponent(questionId)}`)
-          .then(res => res.ok ? res.json() : Promise.reject('Rupert returned an error'))
-          .then(({data})=> store.dispatchAndLog(NoteActions.updateContent(note.id, generateNoteContent(data))));
+      if (note.type !== ModuleEngineType.Rupert) {
+        return Promise.resolve();
       }
 
-      return Promise.resolve();
+      return fetch(`${baseRupertApiUrl}/question/${encodeURIComponent(questionId)}`)
+        .then(res => res.ok ? res.json() : Promise.reject('Rupert responded with an error'))
+        .then(({data})=> store.dispatchAndLog(NoteActions.updateContent(note.id, generateNoteContent(questionId, data))));
+    });
+    
+    hooks.runFinish.tapPromise('RupertNotePlugin', (_app: App, store: Store, note: INote, runner: Runner) => {
+      if (note.type !== ModuleEngineType.Rupert) {
+        return Promise.resolve();
+      }
+
+      const questionId = 'test';
+      const error = runner.getState().getError();
+      const sql = new ParamParser(new DefaultParamSerializer()).format(note.content, [], {
+        customParamsEnabled: true,
+        keepEmbed: false,
+      });
+
+      const payload = {
+        execution_time: Date.now() - runner.getState().getTime().started,
+        rows: sql.split('\n').length,
+        failure: error ? 1 : 0,
+        failure_reason: error.msg,
+        extra_data: {
+          note_id: note.id,
+          note_contents: sql,
+        },
+      };
+
+      return fetch(`${baseRupertApiUrl}/question/${encodeURIComponent(questionId)}/statistics`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+      });
     });
   }
 }
