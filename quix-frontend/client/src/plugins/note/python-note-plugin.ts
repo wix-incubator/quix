f9c115d9c906669748dc2496b57d1059ef75f5f8
Valery Frolov <valeryf@wix.com>
2019-11-23 01:15:05 +0200

    add python note to quix-frontend

---
+++ quix-frontend/client/src/plugins/note/python-note-plugin.ts
@@ -0,0 +1,11 @@
+import { App } from '../../lib/app';
+import {NotePlugin} from '../../services/plugins';
+
+export class PythonNotePlugin extends NotePlugin {
+  constructor(app: App, name: string, hooks: any) {
+    super(app, name, hooks, {
+      syntaxValidation: false,
+      canCreate: true,
+    });
+  }
+}
