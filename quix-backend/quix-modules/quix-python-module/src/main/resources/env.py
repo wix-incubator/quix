45ff9900e930bc8e43ccb9fa928f47e865dda9a0
Valery Frolov <valeryf@wix.com>
2019-11-30 23:01:54 +0200

    minor

--- quix-backend/quix-modules/quix-python-module/src/main/resources/env.py
+++ quix-backend/quix-modules/quix-python-module/src/main/resources/env.py
@@ -39,7 +39,7 @@ def init(dir, required):
             pipmain.main(pipargs)
         except:
             from pip._internal import main as pipmain
-            pipmain.main(pipargs)
+            pipmain(pipargs)
 
         modules_file = open(dir + '/modules', 'w+')
         modules_file.write(' '.join(required))
