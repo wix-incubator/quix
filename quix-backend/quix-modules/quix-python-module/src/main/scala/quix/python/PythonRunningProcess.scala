9252e15c41483e63be4196367302480617fc0866
Valery Frolov <valeryf@wix.com>
2019-12-05 00:41:50 +0200

    - more logging

--- quix-backend/quix-modules/quix-python-module/src/main/scala/quix/python/PythonRunningProcess.scala
+++ quix-backend/quix-modules/quix-python-module/src/main/scala/quix/python/PythonRunningProcess.scala
@@ -6,25 +6,31 @@ import com.typesafe.scalalogging.LazyLogging
 import com.zaxxer.nuprocess.NuProcess
 import monix.eval.Task
 import py4j.GatewayServer
+import quix.core.utils.TaskOps._
 
 case class PythonRunningProcess(queryId: String, var gatewayServer: Option[GatewayServer] = None,
                                 var bridge: Option[PythonBridge] = None,
                                 var process: Option[NuProcess] = None,
                                 var file: Option[Path] = None) extends LazyLogging {
   def close: Task[Unit] = {
-    for {
-      _ <- Task(logger.info(s"method=close event=cleanup-start query-id=${queryId}"))
+    val task = for {
+      _ <- Task(logger.info(s"method=close event=cleanup-start query-id=$queryId"))
 
-      _ <- Task(gatewayServer.map(_.shutdown()))
-        .flatMap(_ => Task(gatewayServer = None)).attempt
+      _ <- Task(gatewayServer.map(_.shutdown())).attempt
+        .flatMap(_ => Task(gatewayServer = None))
 
-      _ <- Task(process.map(_.destroy(true)))
-        .flatMap(_ => Task(process = None)).attempt
+      _ <- Task(process.map(_.destroy(true))).attempt
+        .flatMap(_ => Task(process = None))
 
-      _ <- Task(file.map(f => Files.deleteIfExists(Paths.get(f.getParent.toString, "quix.py"))))
+      _ <- Task(file.map(f => Files.deleteIfExists(Paths.get(f.getParent.toString, "quix.py")))).attempt
+
       _ <- Task(file.map(Files.deleteIfExists)).attempt
-      _ <- Task(file.map(_.getParent).map(Files.deleteIfExists))
-        .flatMap(_ => Task(file = None)).attempt
+
+      _ <- Task(file.map(_.getParent).map(Files.deleteIfExists)).attempt
+        .flatMap(_ => Task(file = None))
+      _ <- Task(logger.info(s"method=close event=cleanup-done query-id=$queryId"))
     } yield ()
+
+    task.logOnError(s"method=close event-cleanup-failure query-id=$queryId")
   }
 }
