660152b775c602792aba634827b91dba6e79c006
Valery Frolov <valeryf@wix.com>
2019-12-16 14:43:56 +0200

    - document some methods

--- quix-backend/quix-modules/quix-python-module/src/main/scala/quix/python/PythonRunningProcess.scala
+++ quix-backend/quix-modules/quix-python-module/src/main/scala/quix/python/PythonRunningProcess.scala
@@ -8,10 +8,24 @@ import monix.eval.Task
 import py4j.GatewayServer
 import quix.core.utils.TaskOps._
 
+/** Python Bridge is py4j bridge to connect between methods defined in quix.py and Quix execution model
+ *
+ * @param queryId       the queryId that started the execution
+ * @param gatewayServer py4j instance for communication between python note and quix
+ * @param bridge        py4j bridge that maps functions exposed in python to quix implementations
+ * @param process       nuprocess instance that executes the python process
+ * @param file          python code that contains all init quix code and actual python note content
+ */
 case class PythonRunningProcess(queryId: String, var gatewayServer: Option[GatewayServer] = None,
                                 var bridge: Option[PythonBridge] = None,
                                 var process: Option[NuProcess] = None,
                                 var file: Option[Path] = None) extends LazyLogging {
+
+  /**
+   * Close all open resources and delete all temporary files
+   *
+   * @return
+   */
   def close: Task[Unit] = {
     val task = for {
       _ <- Task(logger.info(s"method=close event=cleanup-start query-id=$queryId"))
