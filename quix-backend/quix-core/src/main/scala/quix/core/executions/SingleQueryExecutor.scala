cbf0767b6f0c064feabbb206e1e511f7fd1c9ab4
Valery Frolov <valeryf@wix.com>
2019-12-23 09:58:12 +0200

    docs : add scaladoc to SingleQueryExecutor.scala

--- quix-backend/quix-core/src/main/scala/quix/core/executions/SingleQueryExecutor.scala
+++ quix-backend/quix-core/src/main/scala/quix/core/executions/SingleQueryExecutor.scala
@@ -7,16 +7,25 @@ import quix.api.execute.{ActiveQuery, AsyncQueryExecutor, Batch}
 import quix.api.users.User
 import quix.core.results.SingleBuilder
 
+/** SingleQueryExecutor exposes useful methods for executing single queries with small amount of rows */
 trait SingleQueryExecutor {
   val queryExecutor: AsyncQueryExecutor[String, Batch]
 
   val user = User("quix-db-tree")
 
-  def executeForSingleColumn(sql: String, delim: String = "") = {
+  /** @param sql  sql that when executed will return small amount of results
+   * @param delim delimiter that will be used to concatenate each row produced by sql
+   * @return list of rows, if row is made of more than one column, they will be concatenated with delim
+   * */
+  def executeForSingleColumn(sql: String, delim: String = ""): Task[List[String]] = {
     executeFor[String](sql, x => x.mkString(delim))
   }
 
-  def executeFor[T](sql: String, resultMapper: List[String] => T) = {
+  /** @param sql         Sql that when executed will return small amount of results
+   * @param resultMapper Function that will be applied to each row
+   * @return Returns list of T produced by applying resultMapper on each row
+   */
+  def executeFor[T](sql: String, resultMapper: List[String] => T): Task[List[T]] = {
     val query = ActiveQuery(UUID.randomUUID().toString, Seq(sql), user)
     val resultBuilder = new SingleBuilder[String]
     for {
