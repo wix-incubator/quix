45bce4e16b9a8a40a28c7c6c1f33ff11c098d9d5
Jonathan Fraimorice <jonathanfr@wix.com>
2020-01-07 14:37:47 +0200

    added scoll support to SortableTable

--- quix-frontend/client/src/lib/ui/components/SortableTable.tsx
+++ quix-frontend/client/src/lib/ui/components/SortableTable.tsx
@@ -21,50 +21,56 @@ export const SortableTable = ({ columns, data, onRowClicked }) => {
   const firstPageRows = rows.slice(0, 20);
 
   return (
-    <div className={"bi-fade-in"}>
-      <table {...getTableProps()} className={"bi-table"}>
-        <thead className="bi-tbl-header">
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  <div className="bi-table-th-content bi-text--ui bi-text--600">
-                    <span className="bi-tbl-sort-icon bi-icon--sm ng-binding bi-primary">
-                      {column.isSorted
-                        ? column.isSortedDesc
-                          ? "arrow_drop_down"
-                          : "arrow_drop_up"
-                        : "unfold_more"}
-                    </span>
-                    <span className="bi-text--600">
-                      {column.render("Header")}
-                    </span>
-                  </div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {firstPageRows.map((row, index) => {
-            prepareRow(row);
-            return (
-              <tr
-                key={index}
-                {...row.getRowProps()}
-                onClick={() => onRowClicked(row.original)}
-                data-hook="table-row"
-              >
-                {row.cells.map(cell => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
+    <div
+      className={
+        "bi-table-container bi-table--nav bi-c-h bi-grow bi-table-sticky-header"
+      }
+    >
+      <div className={"bi-fade-in"}>
+        <table {...getTableProps()} className={"bi-table"}>
+          <thead className="bi-tbl-header">
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    <div className="bi-table-th-content bi-text--ui bi-text--600">
+                      <span className="bi-tbl-sort-icon bi-icon--sm ng-binding bi-primary">
+                        {column.isSorted
+                          ? column.isSortedDesc
+                            ? "arrow_drop_down"
+                            : "arrow_drop_up"
+                          : "unfold_more"}
+                      </span>
+                      <span className="bi-text--600">
+                        {column.render("Header")}
+                      </span>
+                    </div>
+                  </th>
+                ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {firstPageRows.map((row, index) => {
+              prepareRow(row);
+              return (
+                <tr
+                  key={index}
+                  {...row.getRowProps()}
+                  onClick={() => onRowClicked(row.original)}
+                  data-hook="table-row"
+                >
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
