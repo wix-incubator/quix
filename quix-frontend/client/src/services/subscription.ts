4696ceb95e0cdb4a0477a3db3e5f1a9693a0a4ce
Anton Podolsky <antonp@wix.com>
2020-01-09 18:53:28 +0200

    fix(frontend): secure socket

--- quix-frontend/client/src/services/subscription.ts
+++ quix-frontend/client/src/services/subscription.ts
@@ -4,7 +4,7 @@ import {Store, sessionId} from '../lib/store';
 import {App} from '../lib/app';
 
 export const subscribeToStateChanges = (app: App<ClientConfigHelper>, store: Store) => {
-  const ws = new WebSocket(`ws://${location.host}/subscription`);
+  const ws = new WebSocket(`wss://${location.host}/subscription`);
 
   const {authCookieName} = app.getConfig().getAuth();
   const cookies = cookie.parse(document.cookie);
