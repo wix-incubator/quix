3f388530c07a78bc7e334c5bcf6493f17e186cf8
Anton Podolsky <antonp@wix.com>
2020-01-14 17:21:16 +0200

    fix(frontend): add playground menu item after the bootstrap hook

--- quix-frontend/client/src/app.ts
+++ quix-frontend/client/src/app.ts
@@ -25,49 +25,52 @@ const appBuilder = create<ClientConfigHelper>({
   id: 'quix',
   title: 'Quix',
 }, {
-    auth: clientConfig.getAuth(),
-    statePrefix: 'base',
-    defaultUrl: '/home',
-    homeState: 'home',
-    logoUrl: `${staticsBaseUrl}assets/logo_big.png`,
-    apiBasePath
-  }, ['bi.app', 'bi.runner', 'bi.fileExplorer'])
-  .config(clientConfig)
-  .plugin('app', plugin => {
-    plugin.components(components);
-    plugin.stateComponents(stateComponents);
-    plugin.store(branches, `${apiBasePath}/api/events`, 'Node');
+  auth: clientConfig.getAuth(),
+  statePrefix: 'base',
+  defaultUrl: '/home',
+  homeState: 'home',
+  logoUrl: `${staticsBaseUrl}assets/logo_big.png`,
+  apiBasePath
+}, ['bi.app', 'bi.runner', 'bi.fileExplorer'])
+.config(clientConfig)
+.plugin('app', plugin => {
+  plugin.components(components);
+  plugin.stateComponents(stateComponents);
+  plugin.store(branches, `${apiBasePath}/api/events`, 'Node');
 
-    plugin.menuItem({name: 'Notebooks', icon: 'description', template: '<quix-files-sidebar class="bi-c bi-grow"></quix-files-sidebar>'});
-    plugin.menuItem({name: 'DB Explorer', icon: 'storage', template: '<quix-db-sidebar class="bi-c-h bi-grow"></quix-db-sidebar>'});
-    plugin.menuItem({name: 'separator'});
-    plugin.menuItem({name: 'Playground', icon: 'code', onToggle: () => openTempQuery(inject('$rootScope'))});
+  plugin.menuItem({name: 'Notebooks', icon: 'description', template: '<quix-files-sidebar class="bi-c bi-grow"></quix-files-sidebar>'});
+  plugin.menuItem({name: 'DB Explorer', icon: 'storage', template: '<quix-db-sidebar class="bi-c-h bi-grow"></quix-db-sidebar>'});
 
-    plugin.onPluginReady((app, store) => {
-      runnerConfig.set({
-        executeBaseUrl,
-        apiBasePath,
-      });
-
-      resourcesConfig.set({
-        apiBasePath
-      });
-
-      clientConfig.getModulesByComponent(ModuleComponentType.Db).forEach(({id, engine}) => {
-        pluginManager.module(ModuleComponentType.Db).plugin(id, engine, app);
-      });
-
-      clientConfig.getModulesByComponent(ModuleComponentType.Note).forEach(({id, engine}) => {
-        pluginManager.module(ModuleComponentType.Note).plugin(id, engine, app);
-      });
-
-      initCache(store);
-      setupNotifications(staticsBaseUrl);
-
-      app.getModule().controller('app', ['$scope', scope => scope.app = app] as any);
+  plugin.onPluginReady((app, store) => {
+    runnerConfig.set({
+      executeBaseUrl,
+      apiBasePath,
     });
+
+    resourcesConfig.set({
+      apiBasePath
+    });
+
+    clientConfig.getModulesByComponent(ModuleComponentType.Db).forEach(({id, engine}) => {
+      pluginManager.module(ModuleComponentType.Db).plugin(id, engine, app);
+    });
+
+    clientConfig.getModulesByComponent(ModuleComponentType.Note).forEach(({id, engine}) => {
+      pluginManager.module(ModuleComponentType.Note).plugin(id, engine, app);
+    });
+
+    initCache(store);
+    setupNotifications(staticsBaseUrl);
+
+    app.getModule().controller('app', ['$scope', scope => scope.app = app] as any);
   });
+});
 
 hooks.bootstrap.call(appBuilder);
+
+appBuilder.plugin('dummy', plugin => {
+  plugin.menuItem({name: 'separator'});
+  plugin.menuItem({name: 'Playground', icon: 'code', onToggle: () => openTempQuery(inject('$rootScope'))});
+});
 
 appBuilder.build();
