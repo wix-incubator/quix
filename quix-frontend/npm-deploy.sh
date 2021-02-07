629019727114b20b7cf65ef31d0bc87b32a5bad0
Aviad Hadad <aviad@sthuck.info>
2021-02-07 16:33:05 +0200

    npm deploy, another try

--- quix-frontend/npm-deploy.sh
+++ quix-frontend/npm-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
-lerna publish from-package -y --loglevel=debug
+lerna publish from-package --no-verify-access -y --loglevel=debug
