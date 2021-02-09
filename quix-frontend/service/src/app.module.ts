80d12c43bb607ecccae4a1da3c8316048dac1df0
Aviad Hadad <aviad@sthuck.info>
2021-02-09 13:47:10 +0200

    frontend-service: version 1.0.5

--- quix-frontend/service/src/app.module.ts
+++ quix-frontend/service/src/app.module.ts
@@ -1,46 +1,13 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {SearchModule} from './modules/search/search.module';
-import {EventSourcingModule} from './modules/event-sourcing/event-sourcing.module';
-import {WebApiModule} from './modules/web-api/web-api.module';
-import {ConfigService, ConfigModule} from './config';
-import {
-  DbFileTreeNode,
-  DbFolder,
-  DbNote,
-  DbNotebook,
-  DbUser,
-  DbFavorites,
-  DbMetadata,
-} from './entities';
-import {DbAction} from './modules/event-sourcing/infrastructure/action-store/entities/db-action.entity';
-import {ProxyDbApiBackend} from './modules/proxy-backend/proxy-backend.module';
+import {BaseModule} from './base.module';
+import {ConfigModule, ConfigService} from './config';
 import {AuthModuleConfiguration} from './modules/auth/auth.module';
 import {AuthTypes} from './modules/auth/types';
-
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [],
-      useFactory: async (cs: ConfigService) =>
-        cs.getDbConnection([
-          DbFileTreeNode,
-          DbFolder,
-          DbNote,
-          DbNotebook,
-          DbAction,
-          DbUser,
-          DbFavorites,
-          DbMetadata,
-        ]),
-      inject: [ConfigService],
-    }),
+    BaseModule,
     ConfigModule.create(),
-    EventSourcingModule,
-    WebApiModule,
-    ProxyDbApiBackend,
-    SearchModule,
     AuthModuleConfiguration.createAsync({
       injects: [ConfigService],
       imports: [],
