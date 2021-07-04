54bda5761bcc76a17345e31f0907bcfbee57699f
Tamir Houri <tamirh@wix.com>
2021-07-04 12:13:01 +0300

    bump version

--- quix-frontend/client/src/lib/ui/filters/highlight.ts
+++ quix-frontend/client/src/lib/ui/filters/highlight.ts
@@ -7,17 +7,6 @@ export default [
       }
       const needles: string[] = typeof needle === 'string' ? [needle] : needle;
 
-
-      // tslint:disable-next-line: restrict-plus-operands
-      // return $sce.trustAsHtml(
-      //   needles.reduce((previousHaystack, currentNeedle) => {
-      //     return previousHaystack.replace(
-      //       new RegExp(`(${currentNeedle})(?!.*<mark>|.*</mark>).*$`, "gi"),
-      //       "<mark>$1</mark>"
-      //     );
-      //   }, "" + haystack)
-      // );
-
       // tslint:disable-next-line: restrict-plus-operands
       return $sce.trustAsHtml(('' + haystack).replace(new RegExp(needles.map(n => `(${n})`).join('|'), 'gi'), match => `<mark>${match}</mark>`));
     };
