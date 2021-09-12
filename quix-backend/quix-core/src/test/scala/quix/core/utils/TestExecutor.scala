c478284ecc808a50b0d01753813679843f7f22f6
Valery Frolov <valeryf@wix.com>
2021-09-12 16:35:32 +0300

    minor

--- quix-backend/quix-core/src/test/scala/quix/core/utils/TestExecutor.scala
+++ quix-backend/quix-core/src/test/scala/quix/core/utils/TestExecutor.scala
@@ -7,16 +7,18 @@ import quix.api.v2.execute.{Builder, Executor, SubQuery}
 import scala.collection.mutable.ListBuffer
 
 class TestExecutor extends Executor with LazyLogging {
+
   val executedQueries = ListBuffer.empty[SubQuery]
   var executionToFailOn = -1
 
   def queries: List[String] = executedQueries.map(_.text).toList
 
   override def execute(query: SubQuery, builder: Builder): Task[Unit] = {
-    val result = if (executedQueries.length == executionToFailOn - 1) {
-      Task(builder.errorSubQuery(query.id, new RuntimeException(s"execution $executionToFailOn should fail"))) *> Task.unit
-    } else
-      Task.unit
+    val result = Task(executedQueries.length == executionToFailOn).flatMap {
+      case true =>
+        builder.errorSubQuery(query.id, new RuntimeException(s"execution $executionToFailOn should fail")) *> Task.unit
+      case _ => Task.unit
+    }
 
     Task(logger.info(s"query : ${query.text}")) *> Task(executedQueries += query) *> result
   }
