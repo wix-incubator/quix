b49b0c167b2649451a777cf8cf623eb37ef94e0e
Tamir Houri <82800212+tamirhr@users.noreply.github.com>
2021-09-13 14:24:58 +0300

    autocomplete: fix imports and convert SqlBase to ts files (#371)
    * fix imports and convert SqlBase to ts files
    * fix ts-lint errors
    * bump client version

--- quix-frontend/client/src/lib/language-parsers/presto-grammar/index.js
+++
@@ -1,4 +0,0 @@
-exports.SqlBaseLexer = require('./lang/presto/SqlBaseLexer');
-exports.SqlBaseListener = require('./lang/presto/SqlBaseListener');
-exports.SqlBaseParser = require('./lang/presto/SqlBaseParser');
-exports.SqlBaseVisitor = require('./lang/presto/SqlBaseVisitor');
---
+++ quix-frontend/client/src/lib/language-parsers/presto-grammar/index.ts
@@ -0,0 +1,9 @@
+import { SqlBaseLexer } from './lang/presto/SqlBaseLexer';
+import { SqlBaseListener } from './lang/presto/SqlBaseListener';
+import { SqlBaseParser } from './lang/presto/SqlBaseParser';
+import { SqlBaseVisitor } from './lang/presto/SqlBaseVisitor';
+
+export { SqlBaseLexer };
+export { SqlBaseListener };
+export { SqlBaseParser };
+export { SqlBaseVisitor };
