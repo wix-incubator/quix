e3ea69e61fe43b421747bace035fcff8356c67f7
Oded Kristianpoller <82767422+odedkwix@users.noreply.github.com>
2021-10-24 09:19:34 +0300

    autocomplete: add wt support (#394)

--- quix-frontend/client/src/lib/sql-autocomplete/adapter/sql-autocomplete-adapter.ts
+++ quix-frontend/client/src/lib/sql-autocomplete/adapter/sql-autocomplete-adapter.ts
@@ -75,6 +75,7 @@ export class SqlAutocompleter implements IAutocompleter {
     for (const extractedTable of extractedTables) {
       const { name, alias, columns, type } = extractedTable;
       columns.forEach((column) => {
+
         const shortColumnName = column.split('.').pop().replace('@WT_COLUMN@', '.');
         columnsNamesMemory.add(shortColumnName);
 
@@ -147,7 +148,9 @@ export class SqlAutocompleter implements IAutocompleter {
     }
     return entities.map((entity) =>
       this.createCompleterItem(
-        entity.type !== 'catalog' ? `${prefix}.${entity.name}` : entity.name,
+        entity.type === 'schema' || entity.type === 'column'
+          ? `${prefix}.${entity.name}`
+          : entity.name,
         entity.type
       )
     );
