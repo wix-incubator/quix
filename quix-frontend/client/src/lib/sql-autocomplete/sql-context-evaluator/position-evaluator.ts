46813a40ea5d264f12ba29db430deb5f7d8d1690
Tamir Houri <tamirh@wix.com>
2021-11-15 18:11:57 +0200

    autocomplete: evaluator documentation

--- quix-frontend/client/src/lib/sql-autocomplete/sql-context-evaluator/position-evaluator.ts
+++ quix-frontend/client/src/lib/sql-autocomplete/sql-context-evaluator/position-evaluator.ts
@@ -1,6 +1,13 @@
 import { evaluateContext } from './context-evaluator';
 import { QueryContext } from './types';
 
+/**
+ * Takes query and position and returns the context of the given position in the query 
+ * @param   {string} input sql query
+ * @param   {number} position to evaluate
+ * 
+ * @returns {QueryContext} the evaluated context
+ */
 export const evaluateContextFromPosition = (
   input: string,
   position: number
@@ -11,13 +18,21 @@ export const evaluateContextFromPosition = (
   return { ...evaluateContext(query, identifier), prefix };
 };
 
+/**
+ * Replace the nearest word to the given position in the input with the identifier
+ * @param   {string} input 
+ * @param   {number} position where to insert
+ * @param   {string} identifier string to be inserted
+ * 
+ * @returns {string} the input after replacement
+ */
 const createQueryToEvaluate = (
   input: string,
   position: number,
   identifier: string
-) => {
+): string => {
   return position > 0 && position < input.length + 1
-    ? removePrefix(input.slice(0, position)) +
+    ? removeLastPrefix(input.slice(0, position)) +
         ' ' +
         identifier +
         ' ' +
@@ -25,7 +40,13 @@ const createQueryToEvaluate = (
     : input;
 };
 
-const removePrefix = (input: string) => {
+/**
+ * Removes the last word in the string matches the format (a-z 0-9 .)
+ * @param   {string} input 
+ * 
+ * @returns {string} the input after removal
+ */
+const removeLastPrefix = (input: string): string => {
   const parts = input.match(/([\w._]+)|([\s,()=<>'`":;*!@#$%^&+-]+)/g);
   if (parts[parts.length - 1].match(/([\w._]+)/)) {
     parts.pop();
@@ -33,6 +54,12 @@ const removePrefix = (input: string) => {
   return parts.join('');
 };
 
+/**
+ * @param   {string} input 
+ * @param   {number} position
+ * 
+ * @returns {string} the nearest prefix that matches the format (a-z 0-9 .) before the given postion
+ */
 const getPrefixByPosition = (input: string, position: number): string => {
   return input
     .slice(0, position)
